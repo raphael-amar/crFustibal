8c77646f5ef986f1d15e208a1824408f0fa1c631
David Olivari <david.olivari@gmail.com>
2012-11-20 11:08:21 +0100

    auto update

--- app/public/js/badge.js
+++ app/public/js/badge.js
@@ -1,25 +1,28 @@
 define(["lib/jquery.flot.threshold"], function() {
-	$.getJSON('data/airlr.json', function(data) {
-
-		var last = new Date();
-		last.setDate(last.getDate() - 20);
-		$.plot($('#airlr'), [{
-			data : data.data,
-			color : "rgb(72,120,168)",
-			bars : {
-				show : true,
-				barWidth : 20 * 60 * 60 * 1000,
-				align : "center",
-				lineWidth : 0,
-			}
-		}], {
-			xaxis : {
-				mode : "time",
-				timeformat : "%d %b",
-				minTickSize : [1, "day"],
-				min : last
-			}
+	function updateData() {
+		$.getJSON('data/airlr.json', function(data) {
+			var last = new Date();
+			last.setDate(last.getDate() - 20);
+			$.plot($('#airlr'), [{
+				data : data.data,
+				color : "rgb(72,120,168)",
+				bars : {
+					show : true,
+					barWidth : 20 * 60 * 60 * 1000,
+					align : "center",
+					lineWidth : 0,
+				}
+			}], {
+				xaxis : {
+					mode : "time",
+					timeformat : "%d %b",
+					minTickSize : [1, "day"],
+					min : last
+				}
+			});
 		});
-	});
+		setTimeout(updateData, 4 * 60 * 60 * 1000);
+	}
+	updateData();
 
 });
